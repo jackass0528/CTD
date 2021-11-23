a16bcf8703244a92cb0470c9e701072950cc605f
cebu taxi driver <cebufooddroid2015@gmail.com>
2021-11-23 19:37:03 +0800

    change my code
    add css file and script to my navbar

---
+++ Script.js
@@ -0,0 +1,8 @@
+const togglebutton = document.getElementsByClassName('toggle-button')[0]
+const navbarlink = document.getElementsByClassName('navbar-link')[0]
+
+togglebutton.addEventListener('click', () => {
+
+    navbarlink.classList.toggle('active')
+
+})
