18e21b323d180a6649f427d342280109e2051b3f
cebu taxi driver <cebufooddroid2015@gmail.com>
2021-11-24 11:33:06 +0800

    commit

--- Script.js
+++ Script.js
@@ -6,3 +6,12 @@ togglebutton.addEventListener('click', () => {
     navbarlink.classList.toggle('active')
 
 })
+
+function myFunction() {
+    var x = document.getElementById("mylink");
+    if (x.className === "navbar-link") {
+      x.className += " responsive";
+    } else {
+      x.className = "mylink";
+    }
+  }
